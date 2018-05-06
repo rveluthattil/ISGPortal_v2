54107464ac0551c384b93ccee0efa245c995c09f
Ramachandran Veluthattil <ramachandran.v@synechron.com>
2018-05-05 19:52:28 -0500

    Initial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { environment } from './../environments/environment';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HttpModule, Http } from '@angular/http';
+import { HttpClientModule } from '@angular/common/http'; 
 
-
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { AppNavbarComponent } from './components/app-navbar/app-navbar.component';
+import { CoursesListComponent } from './components/courses-list/courses-list.component';
+import { HomeComponent } from './components/home/home.component';
+import { ServerDetailsComponent } from './components/server-details/server-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+	  AppNavbarComponent,
+    CoursesListComponent,
+    HomeComponent,
+    ServerDetailsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule,
+    HttpClientModule,
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule,
+    NgbModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+		apiKey: "AIzaSyDB076Hy9J9D7Hic6sOueuho2JkZa6uDr0",
+		authDomain: "isghub-71434.firebaseapp.com",
+		databaseURL: "https://isghub-71434.firebaseio.com",
+		projectId: "isghub-71434",
+		storageBucket: "",
+		messagingSenderId: "684300043263"
+	}
 };
